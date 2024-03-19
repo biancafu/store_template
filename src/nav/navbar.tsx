9904923520aacc1fb639fa4166f061572bc97689
Bianca Fu <0509biancafu@gmail.com>
2024-03-19 00:11:01 -0600

    added logo and user location on nav bar, removed previous css import

--- src/nav/navbar.tsx
+++ src/nav/navbar.tsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-
+import './navbar.css'
 function Navbar() {
 //   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a>Home</a>
-        <a>Products</a>
-        <a>Contact</a>
+      <div className='nav'>
+        <div className='nav_item'>Angryy Avocado</div>
+        <div>
+          <a className='nav_a'>Home</a>
+          <a className='nav_a'>Products</a>
+          <a className='nav_a'>Contact</a>
+        </div>
+        <div className='nav_item'>user</div>
       </div>
     </>
   )
