8f6db721e7016079d29da23ae38e20beda75f4d3
Bianca Fu <0509biancafu@gmail.com>
2024-03-18 23:51:07 -0600

    added navbar

---
+++ src/nav/navbar.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+function Navbar() {
+//   const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a>Home</a>
+        <a>Products</a>
+        <a>Contact</a>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
